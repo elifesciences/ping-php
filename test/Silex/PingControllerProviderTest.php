3445362473316923b2b1c6056791bf479f21630f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-09-18 14:33:56 +0100

    Add Silex provider

---
+++ test/Silex/PingControllerProviderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace test\eLife\Ping\Silex;
+
+use eLife\Ping\PingController;
+use eLife\Ping\Silex\PingControllerProvider;
+use Silex\Application;
+use Silex\WebTestCase;
+
+final class PingControllerProviderTest extends WebTestCase
+{
+    /** @var Application */
+    protected $app;
+
+    /**
+     * @test
+     */
+    public function it_creates_a_service()
+    {
+        $this->assertArrayHasKey('ping.controller', $this->app);
+        $this->assertInstanceOf(PingController::class, $this->app['ping.controller']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_registers_a_route()
+    {
+        $this->assertSame('/ping', $this->app['url_generator']->generate('ping'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_responds()
+    {
+        $client = $this->createClient();
+
+        $client->request('GET', '/ping');
+        $response = $client->getResponse();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('pong', $response->getContent());
+    }
+
+    final public function createApplication() : Application
+    {
+        $app = new Application();
+        $app->register(new PingControllerProvider());
+
+        $app['debug'] = true;
+        unset($app['exception_handler']);
+
+        $app->boot();
+        $app->flush();
+
+        return $app;
+    }
+}
