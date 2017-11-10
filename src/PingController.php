67b07aa96ea86ba262574d9fdfc0401fe3d4f05f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-11-10 10:23:57 +0000

    Allow PingController to check things are working

--- src/PingController.php
+++ src/PingController.php
@@ -2,12 +2,35 @@
 
 namespace eLife\Ping;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 final class PingController
 {
+    private $check;
+    private $logger;
+
+    public function __construct(callable $check = null, LoggerInterface $logger = null)
+    {
+        $this->check = $check;
+        $this->logger = $logger;
+    }
+
     public function pingAction() : Response
     {
+        if ($this->check) {
+            try {
+                call_user_func($this->check);
+            } catch (Throwable $e) {
+                if ($this->logger) {
+                    $this->logger->critical('Ping failed', ['exception' => $e]);
+                }
+
+                return $this->createResponse(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+
         return $this->createResponse(Response::HTTP_OK, 'pong');
     }
 
