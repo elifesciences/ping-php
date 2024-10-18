08abe811fa67f6dab148f49a0fffa2305cbb09af
Scott Aubrey <scott@aubrey.org.uk>
2024-10-18 08:18:54 +0100

    Stop testing the library with the lowest dependencies
    While the goal is laudable, by we arbitrarily restrict the window of
    supported versions of PHP by allowing the lowest supported versions of a
    library's dependency (which may have an even lower ceiling than us) to
    dictate our maximum supported version via the test suite.
    
    Since we're now testing against multiple versions of PHP, that will
    provide a better show of our range of supported PHP versions, and it's
    down to consuming libraries to make sure they are compatible with any
    sub-dependency requirements

--- project_tests.sh
+++ project_tests.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-: "${dependencies:?Need to set dependencies environment variable}"
-if [ "$dependencies" = "lowest" ]; then
-    composer update --prefer-lowest --no-interaction
-    vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p src/ test/
-else
-    composer update --no-interaction
-fi
+vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p src/ test/
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
