57aaa3ee5d1c5c62078e08b5e2d50c01671d15b0
Scott Aubrey <scott@aubrey.org.uk>
2024-12-13 15:41:38 +0000

    add pimpleprovider support for the Psr\PingController

--- src/Pimple/PingControllerProvider.php
+++ src/Pimple/PingControllerProvider.php
@@ -3,8 +3,10 @@
 namespace eLife\Ping\Pimple;
 
 use eLife\Ping\PingController;
+use eLife\Ping\Psr\PingController as PsrPingController;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use RuntimeException;
 
 final class PingControllerProvider implements ServiceProviderInterface
 {
@@ -12,6 +14,13 @@ final class PingControllerProvider implements ServiceProviderInterface
     {
         $app['ping.controller'] = function () {
             return new PingController();
+        };
+
+        $app['ping.psrcontroller'] = function () use ($app) {
+            if (!isset($app['psr17factory'])) {
+                throw new RuntimeException('You must register a PSR-17 factory at `psr17factory` before using this service provider.');
+            }
+            return new PsrPingController($app['psr17factory'], $app['psr17factory']);
         };
     }
 }
